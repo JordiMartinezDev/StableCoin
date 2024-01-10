e91743f5d7f742b8293fa45e485b815cff968f2d
JordiMartinezDev <jordiupc87@gmail.com>
2024-01-10 12:30:46 +0100

    overrided burnable function

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,6 +23,11 @@
 // private
 // view & pure functions
 
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
 pragma solidity ^0.8.18;
 
 /**
@@ -35,11 +40,21 @@ pragma solidity ^0.8.18;
  * This contract is meant to be governed by DSCEngine. This contract is just the ERC20 implementation
  of our stablecoin system
  */
-contract DecentralizedStableCoin{
+contract DecentralizedStableCoin is ERC20Burnable, Ownable{
     
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    
+    constructor() ERC20("DecentralizedStableCoin","DSC"){
 
-    constructor(){
-        
     }
 
+    function burnable(uint256 _amount) public override onlyOwner(){
+
+        if(_amount <= 0) revert DecentralizedStableCoin__MustBeMoreThanZero();
+
+        if(balance < _amount) revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+        super.burn(_amount);
+    }
 }

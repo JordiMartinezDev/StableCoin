6dd35377a771741175ee0600e4966ebf62949ce5
JordiMartinezDev <jordiupc87@gmail.com>
2024-01-10 17:14:38 +0100

    depositCollateral modifiers

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -23,63 +23,69 @@
 // private
 // view & pure functions
 
-
-
 pragma solidity ^0.8.18;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title DSCEngine
  * @author Patrick Collins - Jordi Martinez Following Patrick's Youtube course
- * 
+ *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1 tokken = 1 USD
- * 
+ *
  * This stablecoin has the properties:
  * -Exogenous Collateral
  * -Dollar pegged
  * -Algorithmically stable
- * 
+ *
  * Similar to DAI, if DAI had no governance, no fees and was only backed by wETH and wBTC
  *
  * This DSC system should always be "overcollaterized". At no point, should the value of all collateral <= the $ backed value of all the DSC
  *
  * @notice This contract is the core of the DSC system. It handles all the logic for mining and redeeming DSC, as well as depositing and withdrawing collateral.
- * @notice This contract is VERY loosely based on the MakerDAO DSS (DAI) system. 
+ * @notice This contract is VERY loosely based on the MakerDAO DSS (DAI) system.
  */
 
-contract DSCEngine{
-
+contract DSCEngine is ReentrancyGuard {
     // ---------- Errors ---------- //
 
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedMustBeSameLength();
-
+    error DSCEngine__NotAllowedToken();
 
     // ---------- State variables ---------- //
 
     mapping(address token => address priceFeed) private s_priceFeeds; // TokenToPriceFeed
     DecentralizedStableCoin private immutable i_dsc;
 
-
-
     // ---------- Modifiers ---------- //
 
-    modifier moreThanZero(uint256 amount){
+    modifier moreThanZero(uint256 amount) {
+        if (amount <= 0) revert DSCEngine__NeedsMoreThanZero();
 
-        if(amount <= 0) revert DSCEngine__NeedsMoreThanZero();
+        _;
+    }
 
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
         _;
     }
 
     // ---------- Functions ---------- //
 
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedMustBeSameLength();
+        }
 
-     constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress){
-
-        if(tokenAddresses.length != priceFeedAddresses.length) revert DSCEngine__TokenAddressesAndPriceFeedMustBeSameLength();
-
-        for(uint256 i=0; i < tokenAddresses.length;i++){
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
@@ -87,33 +93,31 @@ contract DSCEngine{
 
     // ---------- External Functions ---------- //
 
-    function depositCollateralAndMintDSC() external{
-
-    }
+    function depositCollateralAndMintDSC() external {}
 
     /**
- * @param tokenCollateralAddress The address of the token to deposit collateral
- * @param amountCollateral The amount of collateral to deposit
- */
-    function depositCollateral(address tokenCollateralAddress,uint256 amountCollateral) external moreThanZero(amountCollateral){
+     * @param tokenCollateralAddress The address of the token to deposit collateral
+     * @param amountCollateral The amount of collateral to deposit
+     */
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    )
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {}
 
-    }
-    function redeemCollateralForDSC() external{
+    function redeemCollateralForDSC() external {}
 
-    }
-    function redeemCollateral() external{}
+    function redeemCollateral() external {}
 
-    function mintDSC() external{}
+    function mintDSC() external {}
 
-    function burnDSC() external{
+    function burnDSC() external {}
 
-    }
+    function liquidate() external {}
 
-    function liquidate() external{
-
-    }
-    
-    function getHealthFactor() external{
-
-    }
+    function getHealthFactor() external {}
 }

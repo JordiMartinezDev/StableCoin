55fb39c7bfc3b9055c1a4c362275ec7b0f72cad1
JordiMartinezDev <jordiupc87@gmail.com>
2024-01-10 11:23:12 +0100

    ERC20 coin empty contract

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title 
+ * @author Patrick Collins - Jordi Martinez Following Patrick's Youtube course
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * This contract is meant to be governed by DSCEngine. This contract is just the ERC20 implementation
+ of our stablecoin system
+ */
+contract DecentralizedStableCoin{
+    
+
+    constructor(){
+        
+    }
+
+}

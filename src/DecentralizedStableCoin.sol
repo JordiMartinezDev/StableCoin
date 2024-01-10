63a3cb5be2948f99ef51dccb1cf0d0be1bebd4d5
JordiMartinezDev <jordiupc87@gmail.com>
2024-01-10 15:55:33 +0100

    StableCoin contract finished

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -44,6 +44,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable{
     
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
     
     constructor() ERC20("DecentralizedStableCoin","DSC"){
 
@@ -56,5 +57,14 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable{
         if(balance < _amount) revert DecentralizedStableCoin__BurnAmountExceedsBalance();
 
         super.burn(_amount);
+    }
+
+    function mint(address _to,uint256 _amount)external onlyOwner returns(bool){
+
+        if(_to == address(0)) revert DecentralizedStableCoin__NotZeroAddress();
+        if(_amount <= 0) revert DecentralizedStableCoin__MustBeMoreThanZero();
+
+        _mint(_to,_amount);
+
     }
 }

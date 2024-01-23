8b651c1a415c4bec5c622f72e94fc044f7928577
Jordi Martinez <jordiupc87@gmail.com>
2024-01-23 18:50:33 +0100

    Unit testing file created + imports

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "../../lib/forge-std/src/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+
+contract DSCEngineTest is Test{
+
+    DeployDSC deployer;
+
+    function setUp() public{
+
+        deployer = new Deployer();
+
+
+    }
+
+}

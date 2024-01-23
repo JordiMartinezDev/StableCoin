dbfa15b2f6d1668d21efb0869918bd24f371f059
Jordi Martinez <jordiupc87@gmail.com>
2024-01-23 18:45:04 +0100

    Deploy script working with HelperConfig

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -6,18 +6,33 @@ pragma solidity ^0.8.18;
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployDSC is Script{
 
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+
     function run() external returns(DecentralizedStableCoin, DSCEngine ){
 
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth,wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
 
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DSCEngine engine = new DSCEngine();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+
+        dsc.transferOwnership(address(engine));
 
         vm.stopBroadcast();
 
+        return(dsc,engine);
     }
 
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -10,6 +10,7 @@ import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script{
 
+
     struct NetworkConfig{
         address wethUsdPriceFeed;
         address wbtcUsdPriceFeed;
@@ -24,7 +25,15 @@ contract HelperConfig is Script{
     int256 public constant ETH_USD_PRICE = 2000e8;
     int256 public constant BTC_USD_PRICE = 1000e8;
 
-    constructor() {}
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
 
     function getSepiliaConfig() public view returns(NetworkConfig memory){
         return NetworkConfig({
@@ -37,7 +46,17 @@ contract HelperConfig is Script{
         });
     }
 
-    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory){
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory anvilNetworkConfig){
 
         if(activeNetworkConfig.wethUsdPriceFeed != address(0)){
             return activeNetworkConfig;
@@ -50,12 +69,21 @@ contract HelperConfig is Script{
         MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
         ERC20Mock wethMock = new ERC20Mock();
 
-        vm.stopBroadcast();
         
         // BTC
 
         MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
         ERC20Mock wbtcMock = new ERC20Mock();
+
+        vm.stopBroadcast();
+
+        anvilNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed), // ETH / USD
+            weth: address(wethMock),
+            wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+            wbtc: address(wbtcMock),
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+        });
     }
 
 }
